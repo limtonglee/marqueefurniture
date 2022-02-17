24c346d200e31261ff80de4b1d01bac43ec778d9
Ddaize <dddaize@gmail.com>
2022-02-17 20:45:38 +0800

    set up rootstore using mobx

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "mobx-react";
+import { useStores } from "./stores/RootStore";
 
 ReactDOM.render(
+  <Provider rootStore={useStores}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
--- src/services/Login.js
+++ src/services/Login.js
@@ -1,2 +1,26 @@
-//for login services 
+//for login services
 
+import { api } from "../services/api";
+import { URL_LOGOUT } from "../services/endpoints";
+
+const encryptPassword = (password) => {
+    //todo
+    return password;
+}
+
+const login = (username, password) => encryptPassword(password)
+.then((data) => {
+    const {encryptedPassword} = data;
+    return api.postAsFormInput(URL_LOGIN, {
+        username,
+        password: encryptedPassword
+    })
+})
+
+export const logout = () =>
+  api.postAsJson(URL_LOGOUT).then((res) => {
+    console.log("logout successfully");
+    return res;
+  });
+
+  export default login;

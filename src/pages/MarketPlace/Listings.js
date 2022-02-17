81c3946b8de59ba266e6ea7d6112a7f0eeb60b88
jarrylyeo <jarrylyeo@gmail.com>
2022-02-17 13:30:13 +0800

    Added in tabs for the different listing in marketplace page

--- src/pages/MarketPlace/Listings.js
+++ src/pages/MarketPlace/Listings.js
@@ -1,14 +1,41 @@
+import * as React from 'react';
 import Button from "@mui/material/Button";
-import { ImageList } from "@mui/material";
+import { Container, ImageList } from "@mui/material";
 import { ImageListItem } from "@mui/material";
 import { ImageListItemBar } from "@mui/material";
 import { itemData } from "../../data/itemData";
 import { IconButton } from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
 import { Link } from "react-router-dom";
+import { Tab } from "@mui/material";
+import { Tabs } from "@mui/material";
+import { Box } from "@mui/material";
 
+//This is the main marketplace page
+/*Things to do:
+Inclusion of the bar to separate the different listings: "Furniture / Design / Services"
+*/
 export const Listings = () => {
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+  setValue(newValue);
+  };
+
   return (
+    <Container>
+      <Box sx={{ width: 'auto', bgcolor: 'background' }}>
+        <Tabs 
+          value={value}  
+          onChange={handleChange} 
+          centered
+          variant='fullWidth'>
+          <Tab label="Furniture" />
+          <Tab label="Service" />
+          <Tab label="Design" />
+       </Tabs>
+      </Box>
     <ImageList sx={{ width: "auto", height: "auto" }}>
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
@@ -36,5 +63,6 @@ export const Listings = () => {
         </ImageListItem>
       ))}
     </ImageList>
+    </Container>
   );
 };

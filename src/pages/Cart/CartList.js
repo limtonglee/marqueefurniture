38f1a20580688509084f32cd95a14f8d95e56750
jarrylyeo <jarrylyeo@gmail.com>
2022-02-21 16:36:47 +0800

    Minor edits on the itemdata, itemdetails and the cartlist

--- src/pages/Cart/CartList.js
+++ src/pages/Cart/CartList.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
 
 const Cart = () => {
   return (

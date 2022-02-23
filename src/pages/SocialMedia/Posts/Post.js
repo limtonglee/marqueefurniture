499d37a0b14fcccf34bd0cf330892499c15335cf
Chinn Fang <chinnfang@gmail.com>
2022-02-23 17:01:51 +0800

    add comment button

--- src/pages/SocialMedia/Posts/Post.js
+++ src/pages/SocialMedia/Posts/Post.js
@@ -10,21 +10,14 @@ import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import Favorite from "@mui/icons-material/Favorite";
 import PushPinOutlinedIcon from "@mui/icons-material/PushPinOutlined";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Comment from "./Comment";
 import Avatar from "@mui/material/Avatar";
-
-import IconButton from "@mui/material/IconButton";
 import SendIcon from "@mui/icons-material/Send";
 import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
-import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 

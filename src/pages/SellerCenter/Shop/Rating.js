772af7ab6574cf6001d4b6b445d51b45b74e702a
Jingyuan <57396768+Jingyuan9860@users.noreply.github.com>
2022-02-27 23:35:35 +0800

    Add reply to ratings

--- src/pages/SellerCenter/Shop/Rating.js
+++ src/pages/SellerCenter/Shop/Rating.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import { Layout } from '../Layout';
+import { user } from "../../../data/currentUserData";
 // material
 import {
     Card,
@@ -13,7 +14,10 @@ import {
     Grid,
     ButtonBase,
     Avatar,
+    Link,
+    TextField,
 } from '@mui/material';
+import SendIcon from "@mui/icons-material/Send";
 
 const Img = styled('img')({
     margin: 'auto',
@@ -22,7 +26,51 @@ const Img = styled('img')({
     maxHeight: '100%',
 });
 
+const commentButtonStyles = {
+    textTransform: "none",
+    color: "grey !important",
+    fontWeight: "normal",
+};
+
+const addReplyStyles = {
+    "&.MuiButton-root": {
+        borderRadius: 1.5,
+    },
+    borderColor: "#2E6B75",
+    color: "#2E6B75",
+    marginTop: "10px",
+    width: "100px",
+    "&:hover": {
+        borderColor: "#F2F2F2",
+    },
+};
+
 export const ShopRating = () => {
+    const handleReply = () => {
+        console.log("add comment");
+        setCommentActivated(true);
+    };
+    const [commentActivated, setCommentActivated] = useState(false);
+    const [comment, setComment] = useState("");
+    const updateComment = (e) => {
+        setComment(e.target.value);
+    };
+    const sendComment = () => {
+        console.log("sendComment");
+        console.log(comment);
+
+        const newId = Math.floor(Math.random() * 100 + 1);
+
+        const newComment = {
+            id: newId,
+            user: user,
+            comment: comment,
+            datetime: "",
+        };
+
+        setComment("");
+    };
+
 
     return (
 
@@ -62,8 +110,10 @@ export const ShopRating = () => {
                 <Grid item xs={4}>
                     Your Reply
                 </Grid>
+            </Grid>
+            <Grid container spacing={2}>
                 <Grid item xs={4}>
-                    <Card>
+                    <Card sx={{ height: '100%' }}>
                         <Grid container spacing={2}>
                             <Grid item>
                                 <ButtonBase sx={{ width: 128, height: 128 }}>
@@ -99,7 +149,7 @@ export const ShopRating = () => {
                     </Card>
                 </Grid>
                 <Grid item xs={4}>
-                    <Card>
+                    <Card sx={{ height: '100%' }}>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm container>
                                 <Grid item xs container direction="column" spacing={2}>
@@ -130,7 +180,7 @@ export const ShopRating = () => {
                     </Card>
                 </Grid>
                 <Grid item xs={4}>
-                    <Card>
+                    <Card sx={{ height: '100%' }}>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm container>
                                 <Grid item xs container direction="column" spacing={2}>
@@ -155,6 +205,130 @@ export const ShopRating = () => {
                                 </Grid>
                             </Grid>
                         </Grid>
+                    </Card>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <Card sx={{ height: '100%' }}>
+                        <Grid container spacing={2}>
+                            <Grid item>
+                                <ButtonBase sx={{ width: 128, height: 128 }}>
+                                    <Img alt="complex" src="https://images.unsplash.com/photo-1540574163026-643ea20ade25" />
+                                </ButtonBase>
+                            </Grid>
+                            <Grid item xs={12} sm container>
+                                <Grid item xs container direction="column" spacing={2}>
+                                    <Grid item xs>
+                                        <Typography gutterBottom variant="subtitle1" component="div">
+                                            Sofa
+                                        </Typography>
+                                        <Typography variant="body2" gutterBottom>
+                                            Variation: Brown
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            ID: 0
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                                            Link to listing
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="subtitle1" component="div">
+                                        $298.99
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card sx={{ height: '100%' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm container>
+                                <Grid item xs container direction="column" spacing={2}>
+                                    <Grid item xs>
+                                        <Avatar
+                                            alt="Remy Sharp"
+                                            src="https://www.pngkey.com/png/detail/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+                                            sx={{ width: 50, height: 50 }}
+                                        />
+                                        <Typography gutterBottom variant="subtitle1" component="div">
+                                            Remy Sharp
+                                        </Typography>
+                                        <Typography variant="body2" gutterBottom>
+                                            Good item, fast delivery
+                                        </Typography>
+                                        <Typography variant="body2" gutterBottom>
+                                            Rating: 5/5
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="subtitle1" component="div">
+                                        Date: 27/2/2022
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card sx={{ height: '100%', display: 'flex', alignItems: "center", justifyContent: "center" }}>
+                        {!commentActivated && (
+                            <Button
+
+
+                            onClick={handleReply}
+                            >
+                                Add reply
+                            </Button>
+                        )}
+                        {commentActivated && (
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <TextField
+                                    id="outlined-multiline-flexible"
+                                    multiline
+                                    maxRows={4}
+                                    value={comment}
+                                    onChange={updateComment}
+                                />
+                                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                                    {comment.length == 0 ? (
+                                        <Button
+                                            endIcon={<SendIcon />}
+                                            variant="outlined"
+                                            onClick={sendComment}
+                                            sx={
+                                                addReplyStyles
+                                            }
+                                            disabled
+                                        >
+                                            Send
+                                        </Button>
+                                    ) : (
+                                        <Button
+                                            endIcon={<SendIcon />}
+                                            variant="outlined"
+                                            onClick={sendComment}
+                                            sx={
+                                                addReplyStyles
+                                            }
+                                        >
+                                            Send
+                                        </Button>
+                                    )}
+                                </Box>
+                            </Box>
+                        )}
                     </Card>
                 </Grid>
             </Grid>

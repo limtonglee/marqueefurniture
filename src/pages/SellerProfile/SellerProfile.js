b9beabd0d88fd4aa54f31f09894139f233724874
jarrylyeo <jarrylyeo@gmail.com>
2022-03-02 16:40:31 +0800

    Super basic seller profile at the moment

--- src/pages/SellerProfile/SellerProfile.js
+++ src/pages/SellerProfile/SellerProfile.js
@@ -1,41 +1,48 @@
-import { Box, Card, Typography, Divider } from "@mui/material";
+import { Box, Card, Typography, Divider, Avatar, Grid } from "@mui/material";
 import * as React from "react";
+import tabitha from "../../assets/images/tabitha.jpg";
 
 const SellerProfile = () => {
   return (
     <Card sx={{ height: "100%" }}>
-      <Box display="flex" justifyContent="space-between" alignItems="center" pt={2} px={2}>
-        <Typography variant="h6" fontWeight="medium" textTransform="capitalize">
-          Username
-        </Typography>
-      </Box>
-      <Box p={2}>
-        <Box mb={2} lineHeight={1}>        
-          <Typography variant="button" color="text" fontWeight="regular">
-            cosyrosie
-          </Typography> 
-          
-        </Box>
-        <Box opacity={0.3}>
-          <Divider />
-        </Box>
-        <Typography variant="h6" fontWeight="medium" textTransform="capitalize">
-          This is a description
-        </Typography>
+        <Grid sx={{p: "16px"}}>
+            <Grid item xs = {4}>
+                <Avatar
+                src={tabitha}
+                alt="profile-image"
+                variant="rounded"
+                shadow="sm"
+                />
+            </Grid>
+            <Grid item xs = {4}>       
+                <Typography color="text" fontWeight="bold">
+                    Cosyrosie
+                </Typography> 
+            </Grid>
+        </Grid>
+        <Box p={2}>
+            
+            <Box opacity={0.3}>
+                <Divider />
+            </Box>
+            
+            <Typography variant="h6" fontWeight="medium" textTransform="capitalize">
+            This is a description
+            </Typography>
 
-        <Box mb={2} lineHeight={1}>
-            <Typography variant="button" color="text" fontWeight="regular">
-            Testing the bio
-            </Typography>  
-        </Box>
-        <Typography variant="h6" fontWeight="medium" textTransform="capitalize">
-          This is a website
-        </Typography>
-        <Box mb={2} lineHeight={1}>  
-            <Typography variant="button" color="text" fontWeight="regular">
-                This is the link
-            </Typography> 
-        </Box>
+            <Box mb={2} lineHeight={1}>
+                <Typography variant="button" color="text" fontWeight="regular">
+                    Testing the bio
+                </Typography>  
+            </Box>
+            <Typography variant="h6" fontWeight="medium" textTransform="capitalize">
+                This is a website
+            </Typography>
+            <Box mb={2} lineHeight={1}>  
+                <Typography variant="button" color="text" fontWeight="regular">
+                    This is the link
+                </Typography> 
+            </Box>
         </Box>
     </Card>
   )

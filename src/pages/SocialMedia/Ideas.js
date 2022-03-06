d6ffbc4ec458af915ffef988ac38c3ef075e3c34
Chinn Fang <chinnfang@gmail.com>
2022-03-06 15:28:47 +0800

    changed spacing for codes... rip

--- src/pages/SocialMedia/Ideas.js
+++ src/pages/SocialMedia/Ideas.js
@@ -11,145 +11,140 @@ import Stack from "@mui/material/Stack";
 import { Link, useNavigate } from "react-router-dom";
 
 const Ideas = () => {
-	const [selectedTags, setSelectedTags] = useState([]);
-	const [tagBin, setTagBin] = useState(new Map());
-	let currentSort = "popular";
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [tagBin, setTagBin] = useState(new Map());
+  let currentSort = "popular";
 
-	const pageStyles = {
-		tags: {
-			p: 2,
-		},
-		sortFilter: {
-			p: 2,
-			display: "flex",
-			justifyContent: "space-between",
-		},
-		masonry: {
-			p: 2,
-		},
-	};
+  const pageStyles = {
+    tags: {
+      p: 2,
+    },
+    sortFilter: {
+      p: 2,
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    masonry: {
+      p: 2,
+    },
+  };
 
-	const handleTag = (tag) => {
-		if (selectedTags.includes(tag)) {
-			// remove tag from selectedTags
-			const newSelectedTags = [...selectedTags].filter(
-				(item) => item !== tag
-			);
-			setSelectedTags(newSelectedTags);
+  const handleTag = (tag) => {
+    if (selectedTags.includes(tag)) {
+      // remove tag from selectedTags
+      const newSelectedTags = [...selectedTags].filter((item) => item !== tag);
+      setSelectedTags(newSelectedTags);
 
-			// update posts lists
-			let newPosts = [...posts];
-			newPosts.push(...tagBin.get(tag));
-			setPosts(newPosts);
-			// console.log("after adding back", posts);
+      // update posts lists
+      let newPosts = [...posts];
+      newPosts.push(...tagBin.get(tag));
+      setPosts(newPosts);
+      // console.log("after adding back", posts);
 
-			// delete bin for tag
-			tagBin.delete(tag);
-		} else {
-			// add tag to selectedTags
-			const newSelectedTags = [...selectedTags];
-			newSelectedTags.push(tag);
-			setSelectedTags(newSelectedTags);
+      // delete bin for tag
+      tagBin.delete(tag);
+    } else {
+      // add tag to selectedTags
+      const newSelectedTags = [...selectedTags];
+      newSelectedTags.push(tag);
+      setSelectedTags(newSelectedTags);
 
-			// create bin for tag
-			tagBin.set(tag, []);
+      // create bin for tag
+      tagBin.set(tag, []);
 
-			// update posts lists
-			setPosts((oldPosts) => {
-				let newPosts = oldPosts.filter((post) => {
-					if (post.tags.flat().includes(tag)) {
-						return true;
-					} else {
-						tagBin.get(tag).push(post); // add to bin for restoration later
-						return false;
-					}
-				});
-				// console.log("after removing", newPosts);
-				return newPosts;
-			});
-		}
-	};
+      // update posts lists
+      setPosts((oldPosts) => {
+        let newPosts = oldPosts.filter((post) => {
+          if (post.tags.flat().includes(tag)) {
+            return true;
+          } else {
+            tagBin.get(tag).push(post); // add to bin for restoration later
+            return false;
+          }
+        });
+        // console.log("after removing", newPosts);
+        return newPosts;
+      });
+    }
+  };
 
-	const [posts, setPosts] = useState(postData);
+  const [posts, setPosts] = useState(postData);
 
-	useEffect(() => {
-		sortFeed();
-	}, []);
+  useEffect(() => {
+    sortFeed();
+  }, []);
 
-	const handleSort = (sortType) => {
-		console.log(sortType);
-		currentSort = sortType;
-		sortFeed();
-	};
+  const handleSort = (sortType) => {
+    console.log(sortType);
+    currentSort = sortType;
+    sortFeed();
+  };
 
-	const sortFeed = () => {
-		if (currentSort === "popular") {
-			// sorting by descending amount of likes
-			let newPosts = [...posts];
-			setPosts(newPosts.sort((a, b) => b.likes.length - a.likes.length));
-		}
-		if (currentSort === "recent") {
-			// fake sort (bec dk how datetime looks like in json): ascending amount of likes
-			let newPosts = [...posts];
-			setPosts(newPosts.sort((a, b) => a.id - b.id));
-		}
-		// if (currentSort === "for you") {
-		// 	// fake sort (bec dk the "for you" logic yet): ascending amount of likes
-		// 	let newPosts = [...posts];
-		// 	setPosts(newPosts.sort((a, b) => a.likes.length - b.likes.length));
-		// }
-	};
+  const sortFeed = () => {
+    if (currentSort === "popular") {
+      // sorting by descending amount of likes
+      let newPosts = [...posts];
+      setPosts(newPosts.sort((a, b) => b.likes.length - a.likes.length));
+    }
+    if (currentSort === "recent") {
+      // fake sort (bec dk how datetime looks like in json): ascending amount of likes
+      let newPosts = [...posts];
+      setPosts(newPosts.sort((a, b) => a.id - b.id));
+    }
+    // if (currentSort === "for you") {
+    // 	// fake sort (bec dk the "for you" logic yet): ascending amount of likes
+    // 	let newPosts = [...posts];
+    // 	setPosts(newPosts.sort((a, b) => a.likes.length - b.likes.length));
+    // }
+  };
 
-	const resetDisplay = () => {
-		setPosts(postData);
-		setSelectedTags([]);
-		setTagBin(new Map());
-	};
+  const resetDisplay = () => {
+    setPosts(postData);
+    setSelectedTags([]);
+    setTagBin(new Map());
+  };
 
-	const addToMoodboardButtonStyles = {
-		"&.MuiButton-root": {
-			borderRadius: 1.5,
-		},
-		borderColor: "#2E6B75",
-		color: "#2E6B75",
-		"&:hover": {
-			borderColor: "#F2F2F2",
-		},
-	};
+  const addToMoodboardButtonStyles = {
+    "&.MuiButton-root": {
+      borderRadius: 1.5,
+    },
+    borderColor: "#2E6B75",
+    color: "#2E6B75",
+    "&:hover": {
+      borderColor: "#F2F2F2",
+    },
+  };
 
-	let navigate = useNavigate();
+  let navigate = useNavigate();
 
-	const handleCreatePost = () => {
-		console.log("handleCreatePost");
-		navigate("/new-idea");
-	};
+  const handleCreatePost = () => {
+    console.log("handleCreatePost");
+    navigate("/new-idea");
+  };
 
-	return (
-		<>
-			<Container sx={{ pt: 2 }}>
-				<Box sx={pageStyles.sortFilter}>
-					<SortButton handleSort={handleSort} />
-					<Stack direction="row" spacing={2}>
-						<Button
-							startIcon={<AddIcon />}
-							variant="outlined"
-							onClick={handleCreatePost}
-							sx={addToMoodboardButtonStyles}
-						>
-							New Post
-						</Button>
-						<FilterButton
-							handleTag={handleTag}
-							resetDisplay={resetDisplay}
-						/>
-					</Stack>
-				</Box>
-				<Box sx={pageStyles.masonry}>
-					<FeedGrid posts={posts} />
-				</Box>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container sx={{ pt: 2 }}>
+        <Box sx={pageStyles.sortFilter}>
+          <SortButton handleSort={handleSort} />
+          <Stack direction="row" spacing={2}>
+            <Button
+              startIcon={<AddIcon />}
+              variant="outlined"
+              onClick={handleCreatePost}
+              sx={addToMoodboardButtonStyles}
+            >
+              New Post
+            </Button>
+            <FilterButton handleTag={handleTag} resetDisplay={resetDisplay} />
+          </Stack>
+        </Box>
+        <Box sx={pageStyles.masonry}>
+          <FeedGrid posts={posts} />
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default Ideas;
--- src/pages/SocialMedia/Posts/Comment.js
+++ src/pages/SocialMedia/Posts/Comment.js
@@ -8,53 +8,49 @@ import Button from "@mui/material/Button";
 const { username } = user;
 
 const Comment = ({ comment, post, setPost }) => {
-	const commentStyles = {
-		username: {
-			fontWeight: "bold",
-		},
-		comment: {
-			color: "black",
-		},
-	};
+  const commentStyles = {
+    username: {
+      fontWeight: "bold",
+    },
+    comment: {
+      color: "black",
+    },
+  };
 
-	const deleteComment = () => {
-		console.log("delete comment");
+  const deleteComment = () => {
+    console.log("delete comment");
 
-		const newPost = { ...post };
-		newPost.comments = [...newPost.comments].filter(
-			(postComment) => postComment.id !== comment.id
-		);
-		setPost(newPost);
-	};
+    const newPost = { ...post };
+    newPost.comments = [...newPost.comments].filter(
+      (postComment) => postComment.id !== comment.id
+    );
+    setPost(newPost);
+  };
 
-	return (
-		<>
-			{comment.user.username === username ? (
-				<Box sx={{ display: "flex", justifyContent: "space-between" }}>
-					<Stack direction="row" spacing={0.5}>
-						<Box sx={commentStyles.username}>
-							{comment.user.username}
-						</Box>
-						<Box sx={commentStyles.comment}>{comment.comment}</Box>
-					</Stack>
-					<Button
-						size="small"
-						sx={{ lineHeight: "normal" }}
-						onClick={deleteComment}
-					>
-						Delete
-					</Button>
-				</Box>
-			) : (
-				<Stack direction="row" spacing={0.5}>
-					<Box sx={commentStyles.username}>
-						{comment.user.username}
-					</Box>
-					<Box sx={commentStyles.comment}>{comment.comment}</Box>
-				</Stack>
-			)}
-		</>
-	);
+  return (
+    <>
+      {comment.user.username === username ? (
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Stack direction="row" spacing={0.5}>
+            <Box sx={commentStyles.username}>{comment.user.username}</Box>
+            <Box sx={commentStyles.comment}>{comment.comment}</Box>
+          </Stack>
+          <Button
+            size="small"
+            sx={{ lineHeight: "normal" }}
+            onClick={deleteComment}
+          >
+            Delete
+          </Button>
+        </Box>
+      ) : (
+        <Stack direction="row" spacing={0.5}>
+          <Box sx={commentStyles.username}>{comment.user.username}</Box>
+          <Box sx={commentStyles.comment}>{comment.comment}</Box>
+        </Stack>
+      )}
+    </>
+  );
 };
 
 export default Comment;

945bbe22790304d5208dbdbd434e24d58cf1d7b6
Ddaize <dddaize@gmail.com>
2022-03-13 16:22:32 +0800

    refractor itemdetails

--- src/pages/MarketPlace/ItemDetails.jsx
+++ src/pages/MarketPlace/ItemDetails.jsx
@@ -5,7 +5,6 @@ import ShareIcon from "@mui/icons-material/Share";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import {
   Avatar,
-  Card,
   CardContent,
   CardHeader,
   CardMedia,
@@ -18,7 +17,7 @@ import {
   ListItemText,
   Rating,
   Stack,
-  Typography,
+  Typography
 } from "@mui/material";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
@@ -82,242 +81,176 @@ export const ItemDetails = () => {
     cartStore.addItems(item);
   };
 
+  const HeadingTypoStyle = {
+    color: "text.secondary",
+    fontWeight: "500",
+    fontSize: 20,
+  };
+
   return (
     <>
-      <Card>
-        <CardContent key={item.id}>
-          <ArrowCircleLeftIcon
-            fontSize="large"
-            onClick={() => navigate(-1)}
-            color="primary"
-          />
-          <Grid container spacing={2}>
-            <Grid item md={6} xs={12}>
-              <CardMedia width="auto" align="center">
-                <img
-                  height="auto"
-                  width="100%"
-                  src={item.image}
-                  alt={item.name}
-                  title={item.name}
-                />
-              </CardMedia>
-              <ImageListItem
-                key={item.image}
-                sx={{
-                  boxShadow: 5,
-                  margin: 2,
-                  padding: 0,
-                  width: 75,
-                  height: 75,
-                }}
-              >
-                <img
-                  src={`${item.image}?w=188&h=188&fit=crop&auto=format`}
-                  alt={item.name}
-                  height="auto"
-                  width="50%"
-                />
-              </ImageListItem>
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <CardContent>
-                <CardHeader
-                  avatar={
-                    <Link
-                      to={`/SellerProfile`}
-                      style={{ textDecoration: "none" }}
-                    >
-                      <Avatar
-                        src={jack}
-                        alt="profile-image"
-                        variant="rounded"
-                        shadow="sm"
-                        sx={{ height: "70px", width: "70px" }}
-                      />
-                    </Link>
-                  }
-                  sx={{ p: 0 }}
-                />
-                <br />
-                <Divider />
-                <br />
-                <Typography
-                  variant="h3"
-                  color="text.secondary"
-                  fontWeight="bold"
-                >
-                  {item.name}
+      <ArrowCircleLeftIcon
+        fontSize="large"
+        onClick={() => navigate(-1)}
+        color="primary"
+      />
+      <Grid container spacing={2}>
+        <Grid item md={6} xs={12}>
+          <CardMedia width="auto" align="center">
+            <img
+              height="auto"
+              width="100%"
+              src={item.image}
+              alt={item.name}
+              title={item.name}
+            />
+          </CardMedia>
+          <ImageListItem
+            key={item.image}
+            sx={{
+              boxShadow: 5,
+              margin: 2,
+              padding: 0,
+              width: 75,
+              height: 75,
+            }}
+          >
+            <img
+              src={`${item.image}?w=188&h=188&fit=crop&auto=format`}
+              alt={item.name}
+              height="auto"
+              width="50%"
+            />
+          </ImageListItem>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <CardContent>
+            <Typography variant="h4" fontWeight="bold">
+              {item.name}
+            </Typography>
+            <Divider />
+            {item.type !== "Design" && item.type !== "Service" && (
+              <>
+                <Typography variant="h3" color="primary.main" fontWeight="bold">
+                  ${item.listingprice.toFixed(2)}
                 </Typography>
 
+                <Typography variant="h3" sx={HeadingTypoStyle}>
+                  Rating:
+                  <br />
+                </Typography>
+                <Rating name="read-only" value={4.5} readOnly precision={0.5} />
                 <br />
+                <ListItem sx={{ p: 0 }}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <LocalShippingIcon sx={{ color: "primary" }} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Shipping Provider"
+                    secondary={item.shippingprovider}
+                  />
+                </ListItem>
                 <Divider />
+                <Typography variant="h3" color={item.variations}>
+                  <Typography variant="h3" sx={HeadingTypoStyle}>
+                    Variation:
+                  </Typography>
+                  {item.variations}
+                </Typography>
+                <Divider />
+              </>
+            )}
+            <Typography variant="h3" sx={HeadingTypoStyle}>
+              Product Specifications:
+            </Typography>
+
+            <Grid container spacing={2} column={8}>
+              <Grid item xs={4}>
+                <Typography variant="overline" color="text.secondary">
+                  Categories:
+                </Typography>
+                <br />
+                <Typography variant="overline" color="text.secondary">
+                  Brand:
+                </Typography>
+                <br />
+                <Typography variant="overline" color="text.secondary">
+                  Warranty:
+                </Typography>
+                <br />
                 {item.type !== "Design" && item.type !== "Service" && (
                   <>
-                    <Typography
-                      variant="h3"
-                      sx={{
-                        color: "text.secondary",
-                        fontWeight: "bold",
-                        fontSize: 20,
-                      }}
-                    >
-                      Rating:
-                      <br />
+                    <Typography variant="overline" color="text.secondary">
+                      Parcel Size:
                     </Typography>
-                    <Rating
-                      name="read-only"
-                      value={4.5}
-                      readOnly
-                      precision={0.5}
-                    />
                     <br />
-                    <ListItem sx={{ p: 0 }}>
-                      <ListItemAvatar>
-                        <Avatar>
-                          <LocalShippingIcon sx={{ color: "primary" }} />
-                        </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText
-                        primary="Shipping Provider"
-                        secondary={item.shippingprovider}
-                      />
-                    </ListItem>
-                    <Divider />
-                    <Typography variant="h3" color={item.variations}>
-                      <Typography variant="h3" color="text.secondary">
-                        Variation:
-                      </Typography>
-                      {item.variations}
+                    <Typography variant="overline" color="text.secondary">
+                      Weight:
                     </Typography>
-                    <Divider />
+                    <br />
+                    <Typography variant="overline" color="text.secondary">
+                      Stock Available:
+                    </Typography>
+                    <br />
+                    <Typography variant="overline" color="text.secondary">
+                      Dimension:
+                    </Typography>
+                    <br />
                   </>
                 )}
+              </Grid>
+              <Grid item xs={4}>
+                <Typography variant="overline" color="text.secondary">
+                  {item.category}
+                </Typography>
                 <br />
-                <Typography variant="h3" color="text.secondary">
-                  Details:
+                <Typography variant="overline" color="text.secondary">
+                  {item.brand}
                 </Typography>
-
-                <Grid container spacing={2} column={8}>
-                  <Grid item xs={4}>
-                    <Typography variant="overline" color="text.secondary">
-                      Categories:
-                    </Typography>
-                    <br />
-                    <Typography variant="overline" color="text.secondary">
-                      Brand:
-                    </Typography>
-                    <br />
-                    <Typography variant="overline" color="text.secondary">
-                      Warranty:
-                    </Typography>
-                    <br />
-                    {item.type !== "Design" && item.type !== "Service" && (
-                      <>
-                        <Typography variant="overline" color="text.secondary">
-                          Parcel Size:
-                        </Typography>
-                        <br />
-                        <Typography variant="overline" color="text.secondary">
-                          Weight:
-                        </Typography>
-                        <br />
-                        <Typography variant="overline" color="text.secondary">
-                          Stock Available:
-                        </Typography>
-                        <br />
-                        <Typography variant="overline" color="text.secondary">
-                          Dimension:
-                        </Typography>
-                        <br />
-                      </>
-                    )}
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Typography variant="overline" color="text.secondary">
-                      {item.category}
-                    </Typography>
-                    <br />
-                    <Typography variant="overline" color="text.secondary">
-                      {item.brand}
-                    </Typography>
-                    <br />
-                    <Typography variant="overline" color="text.secondary">
-                      {item.warrantyinfo}
-                    </Typography>
-                    <br />
-                    {item.type !== "Design" && item.type !== "Service" && (
-                      <>
-                        <Typography variant="overline" color="text.secondary">
-                          {item.parcelsize}
-                        </Typography>
-                        <br />
-                        <Typography variant="overline" color="text.secondary">
-                          {item.weight}
-                        </Typography>
-                        <br />
-                        <Typography variant="overline" color="text.secondary">
-                          {item.stockavailable}
-                        </Typography>
-                        <br />
-                        <Typography variant="overline" color="text.secondary">
-                          {item.dimensions}
-                        </Typography>
-                        <br />
-                      </>
-                    )}
-                  </Grid>
-                </Grid>
                 <br />
-                <Typography
-                  variant="h3"
-                  color="text.secondary"
-                  fontWeight="bold"
-                >
-                  Description
+                <Typography variant="overline" color="text.secondary">
+                  {item.warrantyinfo}
                 </Typography>
-                <Typography variant="body1" color="text.secondary">
-                  {item.description}
-                </Typography>
-              </CardContent>
-              <Stack direction="row" spacing={2}>
-                <Fab size="small" color="secondary">
-                  <ShareIcon
-                    onClick={handleClick({
-                      vertical: "top",
-                      horizontal: "center",
-                    })}
-                    color="primary"
-                  />
-                </Fab>
-                {!!userStore.isLoggedIn ? (
+                <br />
+                {item.type !== "Design" && item.type !== "Service" && (
                   <>
-                    <Button
-                      variant="outlined"
-                      onClick={() => {
-                        handleAddCart(item, {
-                          vertical: "top",
-                          horizontal: "center",
-                        });
-                      }}
-                      startIcon={<ShoppingCartIcon />}
-                    >
-                      Add to cart
-                    </Button>
-                    <ToastContainer />
-                    <Button
-                      variant="outlined"
-                      startIcon={<ChatIcon />}
-                      disableElevation
-                      href="http://localhost:3000/chat"
-                    >
-                      Chat
-                    </Button>
+                    <Typography variant="overline" color="text.secondary">
+                      {item.parcelsize}
+                    </Typography>
+                    <br />
+                    <Typography variant="overline" color="text.secondary">
+                      {item.weight}
+                    </Typography>
+                    <br />
+                    <Typography variant="overline" color="text.secondary">
+                      {item.stockavailable}
+                    </Typography>
+                    <br />
+                    <Typography variant="overline" color="text.secondary">
+                      {item.dimensions}
+                    </Typography>
+                    <br />
                   </>
-                ) : (
+                )}
+              </Grid>
+            </Grid>
+            <br />
+            <Divider />
+            <Typography variant="h3" sx={HeadingTypoStyle}>
+              Description
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {item.description}
+            </Typography>
+            <Divider />
+
+            <Stack direction="row" spacing={1} mt={2}>
+              {!!userStore.isLoggedIn ? (
+                <>
                   <Button
                     variant="outlined"
-                    disabled
                     onClick={() => {
                       handleAddCart(item, {
                         vertical: "top",
@@ -328,12 +261,59 @@ export const ItemDetails = () => {
                   >
                     Add to cart
                   </Button>
-                )}
-              </Stack>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
+                  <ToastContainer />
+                  <Button
+                    variant="outlined"
+                    startIcon={<ChatIcon />}
+                    disableElevation
+                    href="http://localhost:3000/chat"
+                  >
+                    Chat
+                  </Button>
+                </>
+              ) : (
+                <Button
+                  variant="outlined"
+                  disabled
+                  onClick={() => {
+                    handleAddCart(item, {
+                      vertical: "top",
+                      horizontal: "center",
+                    });
+                  }}
+                  startIcon={<ShoppingCartIcon />}
+                >
+                  Add to cart
+                </Button>
+              )}
+              <Fab size="small" color="secondary"  >
+                <ShareIcon
+                  onClick={handleClick({
+                    vertical: "top",
+                    horizontal: "center",
+                  })}
+                  color="primary"
+                />
+              </Fab>
+            </Stack>
+          </CardContent>
+        </Grid>
+      </Grid>
+      <Divider></Divider>
+      <CardHeader
+        avatar={
+          <Link to={`/SellerProfile`} style={{ textDecoration: "none" }}>
+            <Avatar
+              src={jack}
+              alt="profile-image"
+              variant="rounded"
+              shadow="sm"
+              sx={{ height: "70px", width: "70px" }}
+            />
+          </Link>
+        }
+        sx={{ p: 0 }}
+      />
     </>
   );
 };

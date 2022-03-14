866eccae9f82a0efa8990b9a222fffb141123ead
jarrylyeo <jarrylyeo@gmail.com>
2022-03-14 20:39:10 +0800

    slight changes to liked listing ui

--- src/pages/Profile/LikedListing/LikedListing.js
+++ src/pages/Profile/LikedListing/LikedListing.js
@@ -1,8 +1,18 @@
+import { Box, Divider, Typography } from '@mui/material'
 import React from 'react'
 
 const LikedListing = () => {
   return (
-    <div>LikedListing</div>
+    <>
+        <Box>
+            <Typography fontWeight = "bold">
+                My Likes
+            </Typography>
+        </Box>
+        <br/>
+        <Divider/>
+        
+    </>
   )
 }
 

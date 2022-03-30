6b975da73ef12b1efc204a0c27d5cb11f32e83d3
Jingyuan <57396768+Jingyuan9860@users.noreply.github.com>
2022-03-31 01:37:22 +0800

    Add box container

--- src/pages/SellerCenter/Layout.js
+++ src/pages/SellerCenter/Layout.js
@@ -1,28 +1,33 @@
 import Sidebar from "./Sidebar";
 import React from 'react';
+import {
+    Box
+} from '@mui/material';
 
 export const Layout = ({ children }) => {
     return (
         <>
-            <h2>Seller Center</h2>
-            <div className='appContainer'
-                style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: "center",
-                    paddingRight: "10%",
-                }}>
-                <div className='sidebar' style={{ width: '25%', marginRight: '20px' }}>
-                    <Sidebar />
+            <Box sx={{paddingLeft: '36px', paddingRight: '36px'}}>
+                <h2>Seller Center</h2>
+                <div className='appContainer'
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: "center",
+                        paddingRight: "10%",
+                    }}>
+                    <div className='sidebar' style={{ width: '25%', marginRight: '20px' }}>
+                        <Sidebar />
+                    </div>
+                    <div style={{ width: '100%' }}>
+                        <main>
+                            <div className="content" >
+                                {children}
+                            </div>
+                        </main>
+                    </div>
                 </div>
-                <div style={{ width: '100%' }}>
-                    <main>
-                        <div className="content" >
-                            {children}
-                        </div>
-                    </main>
-                </div>
-            </div>
+            </Box>
         </>
     );
 }
